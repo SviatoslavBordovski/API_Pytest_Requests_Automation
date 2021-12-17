23b0ac7360e849c5b271b7335a2fdc2df5108cd1
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-12-17 23:25:07 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='ApiPytestAutomation',
+      version='1.0',
+      description="API Pytest automation testing",
+      author='Sviatoslav Bordovski',
+      author_email='sviatkey@gmail.com',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+          "pytest==6.2.4",
+          "pytest-html==2.1.1",
+          "requests==2.23.0",
+          "requests-oauthlib==1.3.0",
+          "PyMySQL==0.9.3",
+          "WooCommerce==2.1.1",
+      ]
+      )
