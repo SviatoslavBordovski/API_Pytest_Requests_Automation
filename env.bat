3052178f1ae90d5596d070bbcccd47aa208fed2f
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-12-17 23:42:27 +0200

    Create env.bat

---
+++ env.bat
@@ -0,0 +1,17 @@
+:: Powershell win10 CLI command to apply all env variables
+:: call env.bat
+
+:: Environment
+set MACHINE=machine1
+set WP_HOST=localhost
+
+:: Verification credentials
+set WC_KEY=your_key
+set WC_SECRET=your_secret
+
+:: Database credentials
+set DB_USER=your_user
+set DB_PASSWORD=your_password
+
+:: Required to resolve 'module not found' issues during tests execution
+set PYTHONPATH=%{PYTHONPATH}%;C:\Users\sbord_iu0ld13\PycharmProjects\ApiPytestRequests\tests_suite
