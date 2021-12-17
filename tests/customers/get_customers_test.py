1229f1a1853617892781a965bd239920a5d99df0
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-12-17 23:30:23 +0200

    Create get_customers_test.py

---
+++ tests/customers/get_customers_test.py
@@ -0,0 +1,11 @@
+import pytest
+from src.utilities.requestsUtility import RequestsUtility
+
+@pytest.mark.customers
+@pytest.mark.tc3
+def test_get_all_customers():
+
+    req_helper = RequestsUtility()
+    rs_api = req_helper.get(endpoint='customers')
+
+    assert rs_api, f"Response of list all customers is empty."
