18a56244a789a7cc247f7ef940d4c8a3a6d8855c
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-01-10 21:32:20 +0200

    Update orders_helper.py

--- src/helpers/orders_helper.py
+++ src/helpers/orders_helper.py
@@ -28,3 +28,39 @@ class OrdersHelper(object):
         api_response = self.request_helper.post('orders', body_params=request_body, expected_status_code=201)
 
         return api_response
+
+    @staticmethod
+    def verify_order_is_created(order_json, exp_cust_id, exp_products):
+        orders_dao = OrdersDAO()
+
+        # verify response
+        assert order_json, f"Create order response is empty."
+        assert order_json['customer_id'] == exp_cust_id, f"Create order with given custoemr id returned" \
+                                                         f"bad customer id.Expected customer_id={exp_cust_id} but got '{order_json['customer_id']}'"
+
+        assert len(order_json['line_items']) == len(exp_products), f"Expected only {len(exp_products)} item in order but " \
+                                                   f"found '{len(order_json['line_items'])}'" \
+                                                   f"Order id: {order_json['id']}."
+
+        # verify db
+        order_id = order_json['id']
+        line_info = orders_dao.get_order_lines_by_order_id(order_id)
+        assert line_info, f"Create order, line item not found in DB. Order id: {order_id}"
+
+        # loop through db table to fetch one order item with 'order_item_type' key that equals 'line_item' value
+        line_items = [i for i in line_info if i['order_item_type'] == 'line_item']
+        # import pdb; pdb.set_trace()
+        assert len(line_items) == 1, f"Expected 1 line item but found {len(line_items)}. Order id: {order_id}"
+
+        # get list of product ids in the response
+        api_product_ids = [i['product_id'] for i in order_json['line_items']]
+
+        for product in exp_products:
+            assert product['product_id'] in api_product_ids, f"Create order does not have at least 1 expected product in DB." \
+                                                             f"Product id: {product['product_id']}. Order id: {order_id}"
+
+    def call_update_an_order(self, order_id, payload):
+        return self.woo_helper.put(f'orders/{order_id}', params=payload)
+
+    def call_retrieve_an_order(self, order_id):
+        return self.woo_helper.get(f"orders/{order_id}")
