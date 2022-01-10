9e00d0cdd20d05f04ee6fb870549bf857d207b87
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-01-10 21:35:40 +0200

    Update orders_helper.py

--- src/helpers/orders_helper.py
+++ src/helpers/orders_helper.py
@@ -57,9 +57,3 @@ class OrdersHelper(object):
         for product in exp_products:
             assert product['product_id'] in api_product_ids, f"Create order does not have at least 1 expected product in DB." \
                                                              f"Product id: {product['product_id']}. Order id: {order_id}"
-
-    def call_update_an_order(self, order_id, payload):
-        return self.woo_helper.put(f'orders/{order_id}', params=payload)
-
-    def call_retrieve_an_order(self, order_id):
-        return self.woo_helper.get(f"orders/{order_id}")
