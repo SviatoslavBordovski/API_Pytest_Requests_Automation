c37035d1763a21da6b3f1a72d289c4669d557b44
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-01-14 22:17:48 +0200

    Update get_products_test.py

--- tests/products/get_products_test.py
+++ tests/products/get_products_test.py
@@ -14,19 +14,19 @@ def test_get_all_products():
 @pytest.mark.tc5
 def test_get_product_by_id():
 
-    # get product data (id, name and status) from the database
+    # Get product data (id, name and status) from the database
     rand_product = ProductsDAO().get_random_product_from_db(1)
     rand_product_id = rand_product[0]['ID']
     product_db_name = rand_product[0]['post_title']
     product_status = rand_product[0]['post_status']
 
-    # make 'get' api call
+    # Make 'get' api call
     product_helper = ProductsHelper()
     rs_api = product_helper.get_product_by_id(rand_product_id)
     api_name = rs_api['name']
     api_post_status = rs_api['status']
 
-    # verify the response for name and product status
+    # Verify the response for name and product status
     assert product_db_name == api_name, f"Get product by id returned wrong product. Product id: {rand_product_id}, " \
                                 f"Db name: {product_db_name}, Api name: {api_name}"
 
