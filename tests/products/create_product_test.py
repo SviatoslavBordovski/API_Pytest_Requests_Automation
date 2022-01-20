d3ea2bc5a614f438ec948781e01dc47e35e81a74
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-01-20 22:35:14 +0200

    Update create_product_test.py

--- tests/products/create_product_test.py
+++ tests/products/create_product_test.py
@@ -17,15 +17,15 @@ def test_create_1_simple_product():
     # Make an api call
     product_rs = ProductsHelper().call_create_product(request_body)
 
-    # verify the response is not empty and actual result matches expected
+    # Verify the response is not empty and actual result matches expected
     assert product_rs, f"Create product api response is empty. Request body is: {request_body}"
     assert product_rs['name'] == request_body['name'], f"Create product api call response has" \
         f"unexpected name. Expected: {request_body['name']}, Actual: {product_rs['name']}"
 
-    # verify a product exists in the db
+    # Verify a product exists in the db
     product_id = product_rs['id']
     db_product = ProductsDAO().get_product_by_id(product_id)
 
-    # verify api product name matches recorded in the db product name
+    # Verify api product name matches recorded in the db product name
     assert request_body['name'] == db_product[0]['post_title'], f"Create product, title in db does not match " \
         f"title in api. DB: {db_product['post_title']}, API: {request_body['name']}"
