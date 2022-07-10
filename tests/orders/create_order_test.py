72fca1ba8f7c8e43defde63a3f29970a9df8169f
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-07-10 21:39:50 +0300

    Update create_order_test.py

--- tests/orders/create_order_test.py
+++ tests/orders/create_order_test.py
@@ -6,11 +6,11 @@ import pytest
 
 @pytest.fixture(scope='module')
 def orders_setup():
-    """This fixture allows to use it for every function/test inside of current module"""
     product_dao = ProductsDAO()
+    order_helper = OrdersHelper()
+    
     rand_product = product_dao.get_random_product_from_db(1)
     product_id = rand_product[0]['ID']
-    order_helper = OrdersHelper()
 
     info = {'product_id': product_id,
             'order_helper': order_helper}
@@ -20,14 +20,14 @@ def orders_setup():
 
 @pytest.mark.orders
 @pytest.mark.tc8
-def test_create_order_guest_user(my_orders_smoke_setup):
+def test_create_order_guest_user(orders_setup):
 
     logger.info("Test Case #8: Create a new paid order by guest user.")
 
-    # prepare test data
     order_helper = orders_setup['order_helper']
+
     customer_id = 0
-    product_id = my_orders_smoke_setup['product_id']
+    product_id = orders_setup['product_id']
 
     # make an api call
     info = {"line_items": [
@@ -38,6 +38,34 @@ def test_create_order_guest_user(my_orders_smoke_setup):
               ]}
     order_json = order_helper.create_order(additional_args=info)
 
-    # verify an api response
+    import pdb; pdb.set_trace()
+    # verify response
+    expected_product = [{'product_id': product_id}]
+    order_helper.verify_order_is_created(order_json, customer_id, expected_product)
+
+@pytest.mark.orders
+@pytest.mark.tc9
+def test_create_paid_order_new_created_customer(my_orders_smoke_setup):
+    # create helper objects
+    order_helper = my_orders_smoke_setup['order_helper']
+    customer_helper = CustomerHelper()
+
+    # make the call
+    cust_info = customer_helper.create_customer()
+    customer_id = cust_info['id']
+    product_id = my_orders_smoke_setup['product_id']
+
+    info = {"line_items": [
+                {
+                  "product_id": product_id,
+                  "quantity": 1
+                }
+              ],
+    "customer_id": customer_id
+    }
+
+    order_json = order_helper.create_order(additional_args=info)
+
+    # verify response
     expected_products = [{'product_id': product_id}]
     order_helper.verify_order_is_created(order_json, customer_id, expected_products)
