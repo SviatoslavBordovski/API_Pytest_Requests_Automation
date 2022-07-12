d17a88b49a673e07a94192e0718566f72d03d1d9
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-07-12 21:01:22 +0300

    Update orders_helper.py

--- src/helpers/orders_helper.py
+++ src/helpers/orders_helper.py
@@ -13,14 +13,14 @@ class OrdersHelper(object):
 
     def create_order(self, additional_args=None):
 
-        # setting request body before the call execution
+        # setting body for the api request
         body_template = os.path.join(self.cur_file_dir, '..', 'data', 'create_order_payload.json')
 
         # open that file and modify it from json to python
         with open(body_template) as f:
             request_body = json.load(f)
 
-        # if user adds more info to the request body, then update it
+        # if user adds more info to payload, then update it
         if additional_args:
             assert isinstance(additional_args, dict), f"Parameter 'additional_args' must be a dictionary but found {type(additional_args)}"
             request_body.update(additional_args)
@@ -35,20 +35,22 @@ class OrdersHelper(object):
 
         # verify response
         assert order_json, f"Create order response is empty."
-        assert order_json['customer_id'] == exp_cust_id, f"Create order with given custoemr id returned" \
-                                                         f"bad customer id.Expected customer_id={exp_cust_id} but got '{order_json['customer_id']}'"
+        assert order_json['customer_id'] == exp_cust_id, f"Create order with given customer id returned " \
+                                                         f"bad customer id. Expected customer_id={exp_cust_id} but got " \
+                                                         f"'{order_json['customer_id']}'"
 
         assert len(order_json['line_items']) == len(exp_products), f"Expected only {len(exp_products)} item in order but " \
-                                                   f"found '{len(order_json['line_items'])}'" \
-                                                   f"Order id: {order_json['id']}."
+                                                                   f"found '{len(order_json['line_items'])}'" \
+                                                                   f"Order id: {order_json['id']}."
 
-        # verify db response, fetch orders by id
+        # verify db
         order_id = order_json['id']
         line_info = orders_dao.get_order_lines_by_order_id(order_id)
         assert line_info, f"Create order, line item not found in DB. Order id: {order_id}"
 
         # loop through db table to fetch one order item with 'order_item_type' key that equals 'line_item' value
         line_items = [i for i in line_info if i['order_item_type'] == 'line_item']
+        # import pdb; pdb.set_trace()
         assert len(line_items) == 1, f"Expected 1 line item but found {len(line_items)}. Order id: {order_id}"
 
         # get list of product ids in the response
