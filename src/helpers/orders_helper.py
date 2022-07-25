77dd1492dd17fd7a0df8d315e16a16367691a2bc
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-07-25 21:19:08 +0300

    Update orders_helper.py

--- src/helpers/orders_helper.py
+++ src/helpers/orders_helper.py
@@ -13,7 +13,7 @@ class OrdersHelper(object):
 
     def create_order(self, additional_args=None):
 
-        # set body for the api request
+        # setting body for the api request
         body_template = os.path.join(self.cur_file_dir, '..', 'data', 'create_order_payload.json')
 
         # open that file and modify it from json to python
@@ -29,7 +29,7 @@ class OrdersHelper(object):
 
         return api_response
 
-    @staticmethod
+    @staticmethod  # Main purpose is to contain logic pertaining to the class, but that logic should not have any need for specific class instance data.
     def verify_order_is_created(order_json, exp_cust_id, exp_products):
         orders_dao = OrdersDAO()
 
@@ -40,22 +40,36 @@ class OrdersHelper(object):
                                                          f"'{order_json['customer_id']}'"
 
         assert len(order_json['line_items']) == len(exp_products), f"Expected only {len(exp_products)} item in order but " \
-                                                                   f"found '{len(order_json['line_items'])}', " \
+                                                                   f"found '{len(order_json['line_items'])}'" \
                                                                    f"Order id: {order_json['id']}."
 
-        # verify record exists in the db
+        # verify db
         order_id = order_json['id']
         line_info = orders_dao.get_order_lines_by_order_id(order_id)
         assert line_info, f"Create order, line item not found in DB. Order id: {order_id}"
 
         # loop through db table to fetch one order item with 'order_item_type' key that equals 'line_item' value
         line_items = [i for i in line_info if i['order_item_type'] == 'line_item']
-        # import pdb; pdb.set_trace()
         assert len(line_items) == 1, f"Expected 1 line item but found {len(line_items)}. Order id: {order_id}"
 
-        # get list of product ids in api response
-        api_product_ids = [i['product_id'] for i in order_json['line_items']]
+        # get one item from the db
+        line_item_id = line_items[0]['order_item_id']
+        line_details = orders_dao.get_order_items_details(line_item_id)
 
+        # verify db product id matches api product id
+        db_product_id = int(line_details['_product_id'])
+        api_product_id = order_json['line_items'][0]['product_id']
+        assert db_product_id == api_product_id, \
+            f"Create order product id in db {db_product_id} does not match product id in api response {api_product_id}"
+
+        # get list of product ids in the response
+        api_product_ids = [i['product_id'] for i in order_json['line_items']]
         for product in exp_products:
             assert product['product_id'] in api_product_ids, f"Create order does not have at least 1 expected product in DB." \
                                                              f"Product id: {product['product_id']}. Order id: {order_id}"
+
+    def call_update_an_order(self, order_id, payload):
+        return self.woo_helper.put(f'orders/{order_id}', params=payload)
+
+    def call_retrieve_an_order(self, order_id):
+        return self.woo_helper.get(f"orders/{order_id}")
